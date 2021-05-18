d7373b23c8674840bb0002a6c3fc7480f31b9b36
liuzf <liuzf@digitalcitylab.com.cn>
2021-05-18 16:12:57 +0800

    feat:eureka

---
+++ eureka-client/src/test/java/com/zfliu/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zfliu.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
